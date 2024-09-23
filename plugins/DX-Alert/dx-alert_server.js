0e78ae29de47ad19a899fda2166a0e510cc168d4
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-09-23 20:52:43 +0200

    Update dx-alert_server.js

--- plugins/DX-Alert/dx-alert_server.js
+++ plugins/DX-Alert/dx-alert_server.js
@@ -1,8 +1,8 @@
 ////////////////////////////////////////////////////////////////
 ///                                                          ///
-///  DX ALERT SERVER SCRIPT FOR FM-DX-WEBSERVER (V3.2b BETA) ///
+///  DX ALERT SERVER SCRIPT FOR FM-DX-WEBSERVER (V3.2b)      ///
 ///                                                          ///
-///  by Highpoint                last update: 13.09.24       ///
+///  by Highpoint                last update: 23.09.24       ///
 ///                                                          ///
 ///  Thanks to _zer0_gravity_ for the Telegram Code!         ///
 ///                                                          ///
