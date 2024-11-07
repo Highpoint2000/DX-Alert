70a331db4745b8487b55aa816781c616696e998c
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-11-07 23:01:00 +0100

    Update DXAlert.js

--- plugins/DXAlert.js
+++ plugins/DXAlert.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'DX Alert',
-    version: '3.4',
+    version: '3.5',
     author: 'Highpoint',
     frontEndPath: 'DX-Alert/dx-alert.js'
 }
