65dfb3e2367c96bd392f753d93ffe2321d39adde
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-11-19 12:22:26 +0100

    Update dx-alert.js

--- DX-Alert/dx-alert.js
+++ DX-Alert/dx-alert.js
@@ -13,7 +13,7 @@
 ///  This plugin only works from web server version 1.2.8.1!!!
 
 (() => {
-    const plugin_version = '3.5';
+    const plugin_version = '3.5a';
     let AlertActive = false;
     let wsSendSocket;
     let pressTimer;
